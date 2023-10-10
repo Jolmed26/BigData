95d8cc6e7743cc433fb31c5dd6e1f3996c28ba09
Jolmed26 <pemed49es@gmail.com>
2023-10-09 22:55:30 -0600

    Hola

---
+++ rdd_txt.py
@@ -0,0 +1,19 @@
+import findspark
+findspark.init()
+
+from pyspark import SparkContext #Páginas web para web scraping
+
+# Crea un contexto Spark
+sc = SparkContext("local", "EjemploRDD")
+
+# Carga un archivo de texto como un RDD
+rdd = sc.textFile("archivo.txt")
+
+# Realiza una transformación: filtra líneas que contienen la palabra "Python"
+rdd_filtrado = rdd.filter(lambda linea: "Python" in linea)
+
+# Realiza una acción: muestra los resultados
+print(rdd_filtrado.collect())
+
+# Detén el contexto Spark
+sc.stop()
